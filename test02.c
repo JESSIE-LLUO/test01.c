889d6c006727e4fafa76075a41b7b70c037aaf67
Your Name <lsj20001023@163.com>
2019-10-15 17:10:10 +0800

    ghfg

---
+++ test02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float radius,volume;
+    radius=4.5;
+    volume=4/3.0*3.14*radius*radius*radius;
+    printf("Volume is %.2f\n",volume);
+    return 0;
+}
